cc9534f1ad951f0941ba0a74b966995131063d7d
Jeremy Villa <villajeremy1991@gmail.com>
2022-12-24 01:06:52 -0500

    Routing successfully setup

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -5,10 +5,12 @@ import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { MaterialModule } from '../shared/material.module';
 import { FormsModule } from '@angular/forms';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
   declarations: [
-    ButtonsComponent
+    ButtonsComponent,
+    FlexboxComponent
   ],
   imports: [
     CommonModule,
